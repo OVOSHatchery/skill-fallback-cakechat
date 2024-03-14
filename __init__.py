f9084376b656808531d8cc03ca64cf8c0103bcf8
JarbasAI <33701864+JarbasAl@users.noreply.github.com>
2024-03-14 20:01:46 +0000

    modernize

--- __init__.py
+++ __init__.py
@@ -12,32 +12,27 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from mycroft import FallbackSkill, intent_handler
+from ovos_workshop.skills.fallback import FallbackSkill
 import requests
-import urllib
 import json
-import os
 
 
 class FallbackCakeChatSkill(FallbackSkill):
-    def __init__(self):
-        FallbackSkill.__init__(self)
-
 
     def initialize(self):
-        self.register_fallback(self.handle_fallback_cakechat, 2)
+        self.register_fallback(self.handle_fallback_cakechat, 80)
 
     def handle_fallback_cakechat(self, message):
         header_content = {'Content-type': 'application/json'}
         utterance = message.data.get('utterance').lower()
         message = [utterance]
         js_data = json.dumps({'context': message, 'emotion': 'neutral'})
-        read_url = 'http://localhost:8080/cakechat_api/v1/actions/get_response'
-        response = requests.post(read_url, data=js_data, headers=header_content, verify=False)
+        host = self.settings.get("host", "http://0.0.0.0:8080")
+        read_url = f'{host}/cakechat_api/v1/actions/get_response'
+        response = requests.post(read_url,
+                                 data=js_data,
+                                 headers=header_content,
+                                 verify=False)
         json_data = json.loads(response.text)
         self.speak(json_data["response"])
         return True
-
-
-def create_skill():
-    return FallbackCakeChatSkill()
